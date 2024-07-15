43d1649819f9548bad1d698800625d7b34c380a8
ckt1022 <gary.chen.1022@gmail.com>
2024-07-16 00:12:52 +0800

    增加邊界、計分，放大貪食蛇

--- main.cpp
+++ main.cpp
@@ -5,7 +5,9 @@
 #include <SFML/System/Vector2.hpp>
 #include <SFML/Window/Keyboard.hpp>
 #include <cstdlib>
+#include <ios>
 #include <iostream>
+#include <string>
 #include <vector>
 #include <random>
 
@@ -14,29 +16,59 @@ using namespace std;
 
 int main()
 {
-	int wigth = 650;
-	int length = 650;
+	int wigth = 800 , length = 850;
+	//創建視窗
     RenderWindow window(VideoMode(wigth,length), "Snake");
-
+	
+	//貪食蛇設定
     vector<RectangleShape> snake;
 	
     RectangleShape food;
-	int snakeSize = 10;
-    food.setSize(Vector2f(snakeSize, snakeSize));
-    food.setPosition(rand() % (wigth/10) * snakeSize, rand() % (wigth/10) * snakeSize);
+	int snakeSize = 20;
+    food.setSize(Vector2f(snakeSize,snakeSize));
+    food.setPosition(rand() % (wigth/snakeSize) * snakeSize, rand() % (wigth/snakeSize) * snakeSize);
     food.setFillColor(Color::Green);
 
     RectangleShape ori;
     ori.setSize(Vector2f(snakeSize, snakeSize));
-    ori.setPosition(wigth/2-snakeSize/2,wigth/2-snakeSize/2);
+    ori.setPosition(wigth/2-snakeSize/2-10,wigth/2-snakeSize/2-10);
     ori.setFillColor(Color::Red);
 
     snake.push_back(food);
     snake.push_back(ori);
 
+	//背景障礙物
+	vector<RectangleShape> wall;
+
+	int wallSize = snakeSize;
+
+	for(int x=0;x<wigth;x+=20){
+		for(int y=0;y<wigth;y+=20){
+			if(x == 0 || x == wigth-wallSize || y == 0 || y == wigth-wallSize){
+				RectangleShape temp;
+				temp.setSize(Vector2f(wallSize,wallSize));
+				temp.setPosition(x,y);
+				temp.setFillColor(Color::Cyan);
+
+				wall.push_back(temp);
+			}
+		}
+	}
+
 	//Game Interface
+	
+	sf::Font font;
+    if (!font.loadFromFile("/usr/share/fonts/truetype/dejavu/DejaVuSans-Bold.ttf")) {
+        return -1; // Handle error
+    }
 
-
+	Text scoreText;
+    scoreText.setFont(font); // Set the font
+	scoreText.setPosition(20,810);
+    scoreText.setCharacterSize(24); // Set the character size (in pixels)
+    scoreText.setFillColor(sf::Color::White); // Set the text color
+	
+	//main window loop
     while (window.isOpen())
     {
         Event event;
@@ -50,19 +82,18 @@ int main()
 		
             if (Keyboard::isKeyPressed(Keyboard::Up) && y - snakeSize >= 0)
                 snake[1].setPosition(x, y - snakeSize);
-            if (Keyboard::isKeyPressed(Keyboard::Down) && y + snakeSize <= wigth-snakeSize)
+			if (Keyboard::isKeyPressed(Keyboard::Down) && y + snakeSize <= wigth-snakeSize)
                 snake[1].setPosition(x, y + snakeSize);
-            if (Keyboard::isKeyPressed(Keyboard::Right) && x + snakeSize <= wigth-snakeSize)
+			if (Keyboard::isKeyPressed(Keyboard::Right) && x + snakeSize <= wigth-snakeSize)
                 snake[1].setPosition(x + snakeSize, y);
-            if (Keyboard::isKeyPressed(Keyboard::Left) && x - snakeSize >= 0)
+			if (Keyboard::isKeyPressed(Keyboard::Left) && x - snakeSize >= 0)
                 snake[1].setPosition(x - snakeSize, y);
-			bool move = false;
 
-			if(snake[1].getPosition().x != x || snake[1].getPosition().y != y)
-				move = true;
+			snake[1].setFillColor(Color::Red);
+			snake[1].setSize(Vector2f(snakeSize,snakeSize));
 
             if (snake[1].getPosition() == snake[0].getPosition()) {
-                snake[0].setPosition(rand() % (wigth/10) * snakeSize, rand() % (wigth/10) * snakeSize);
+                snake[0].setPosition(rand() % (wigth/snakeSize) * snakeSize, rand() % (wigth/snakeSize) * snakeSize);
 
                 RectangleShape tail;
                 tail.setSize(Vector2f(snakeSize, snakeSize));
@@ -70,20 +101,22 @@ int main()
                 tail.setPosition(snake[snake.size() - 1].getPosition());
                 snake.push_back(tail);
             }
-			if(move){	
+			if(snake[1].getPosition().x != x || snake[1].getPosition().y != y){	
             	for (int i = snake.size() - 1; i > 2; i--) {
                 	snake[i].setPosition(snake[i-1].getPosition());
             	}
             	snake[2].setPosition(x, y);
 			}
-
-		//	scoreText.setString("Score: " + to_string(snake.size()-2));
+			scoreText.setString("Score: " + to_string(snake.size()-2) + "	" + "Level: " + to_string((snake.size()-2) / 5));
         }
 
         window.clear();
         for (int i = 0; i < snake.size(); i++)
             window.draw(snake[i]);
-		//window.draw(scoreText);
+		for(int i=0;i<wall.size();i++){
+			window.draw(wall[i]);
+		}
+		window.draw(scoreText);
         window.display();
     }
 

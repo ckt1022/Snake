40daa99870db92ccf1bcfb4e7051305f90c9908a
ckt1022 <gary.chen.1022@gmail.com>
2024-07-17 00:26:10 +0800

    邊界觸碰、GameOver介面、重新遊戲

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
-#include <random>
+#include <ctime>
 
 using namespace sf;
 using namespace std;
@@ -24,7 +24,7 @@ vector<RectangleShape> generateWall(int wallSize,int wigth,int length,int id){
 		wall.push_back(temp);
 
 	}
-	if(id == 1){
+	if(id == 0){
 		for(int x=0;x<wigth;x+=wallSize){
 			for(int y=0;y<length - wallSize;y+=wallSize){
 				if(x == 0 || x == wigth-wallSize || y == 0 || y == length-wallSize){
@@ -37,7 +37,7 @@ vector<RectangleShape> generateWall(int wallSize,int wigth,int length,int id){
 				}
 			}
 		}
-	}else if(id == 2){
+	}else if(id == 1){
 		for(int x = 0;x < wigth;x += wallSize){
 			for(int y = 0;y<wigth;y+=wallSize){
 				if((x == 100 && y > 100) || (y == 600 && x > 300) || (y == 100 && (x >=100 && x < 500))){
@@ -54,13 +54,23 @@ vector<RectangleShape> generateWall(int wallSize,int wigth,int length,int id){
 	}
 	return wall;
 }
+
+bool checkValidStep(vector<RectangleShape> wall,int x,int y){
+	for(int i=0;i<wall.size();i++){
+		if(wall[i].getPosition().x == x && wall[i].getPosition().y == y)
+			return false;
+	}
+	return true;
+}
 int main()
 {
+	srand(time(NULL));
 	int wigth = 800 , length = 850;
 	//創建視窗
     RenderWindow window(VideoMode(wigth,length), "Snake");
 	
 	//貪食蛇設定
+	bool gameOver = false;
     vector<RectangleShape> snake;
 	
     RectangleShape food;
@@ -77,48 +87,66 @@ int main()
     snake.push_back(food);
     snake.push_back(ori);
 
-	//背景障礙物
-	vector<RectangleShape> wall = generateWall(snakeSize,wigth,wigth,2);
-	//Game Interface
 	
-	sf::Font font;
+	//字體
+	Font font;
     if (!font.loadFromFile("/usr/share/fonts/truetype/dejavu/DejaVuSerif-Bold.ttf")) {
         return -1; // Handle error
     }
 
+	//顯示訊息設定
 	Text scoreText;
-    scoreText.setFont(font); // Set the font
+    scoreText.setFont(font);
 	scoreText.setPosition(20,810);
-    scoreText.setCharacterSize(24); // Set the character size (in pixels)
-    scoreText.setFillColor(Color::White); // Set the text color
+    scoreText.setCharacterSize(24);
+    scoreText.setFillColor(Color::White);
 	
+	Text gameText("Game Over !!",font,60);
+	gameText.setPosition(175,350);
+	gameText.setFillColor(Color::Red);
+
+	Text endText("Pressed 'Space' to start a new game",font,25);
+	endText.setPosition(150,425);
+	endText.setFillColor(Color::Blue);
+
 	//main window loop
     while (window.isOpen())
     {
+		vector<RectangleShape> wall = generateWall(snakeSize,wigth,wigth,(snake.size()-2)/5);
         Event event;
-		bool gameOver = false;
-        while (window.pollEvent(event))
+	    while (window.pollEvent(event))
         {
+			if(gameOver && Keyboard::isKeyPressed(Keyboard::Space)){
+				snake.erase(snake.begin()+2,snake.end());
+				snake[1].setSize(Vector2f(snakeSize, snakeSize));
+    			snake[1].setPosition(wigth/2-snakeSize/2-10,wigth/2-snakeSize/2-10);
+    			snake[1].setFillColor(Color::Red);
+				
+				int tempX,tempY;
+				do{
+					tempX = rand() % (wigth/snakeSize) * snakeSize;
+					tempY = rand() % (wigth/snakeSize) * snakeSize;
+				}while(!checkValidStep(wall,tempX,tempY));
+					
+				snake[0].setPosition(tempX,tempY);
+
+				gameOver = false;
+			}
             if (event.type == Event::Closed)
                 window.close();
 			
 			//snake head move
-			int lastMove = -1;
             int x = snake[1].getPosition().x;
             int y = snake[1].getPosition().y;
 		
             if (Keyboard::isKeyPressed(Keyboard::Up) && y - snakeSize >= 0){
                 snake[1].setPosition(x, y - snakeSize);
-				lastMove = 1;
 			}else if (Keyboard::isKeyPressed(Keyboard::Down) && y + snakeSize <= wigth-snakeSize){
                 snake[1].setPosition(x, y + snakeSize);
-				lastMove = 2;
 			}else if (Keyboard::isKeyPressed(Keyboard::Right) && x + snakeSize <= wigth-snakeSize){
                 snake[1].setPosition(x + snakeSize, y);
-				lastMove = 3;
 			}else if (Keyboard::isKeyPressed(Keyboard::Left) && x - snakeSize >= 0){
                 snake[1].setPosition(x - snakeSize, y);
-				lastMove = 4;
 			}
 
 			snake[1].setFillColor(Color::Red);
@@ -126,7 +154,13 @@ int main()
 			
 			//when snake meet food
             if (snake[1].getPosition() == snake[0].getPosition()) {
-                snake[0].setPosition(rand() % (wigth/snakeSize) * snakeSize, rand() % (wigth/snakeSize) * snakeSize);
+				int tempX,tempY;
+				do{
+					tempX = rand() % (wigth/snakeSize) * snakeSize;
+					tempY = rand() % (wigth/snakeSize) * snakeSize;
+				}while(!checkValidStep(wall,tempX,tempY));
+					
+				snake[0].setPosition(tempX,tempY);
 
                 RectangleShape tail;
                 tail.setSize(Vector2f(snakeSize, snakeSize));
@@ -143,25 +177,37 @@ int main()
             	snake[2].setPosition(x, y);
 			}
 
-			for(int i=2;i<snake.size();i++){
-				if(snake[i].getPosition() == snake[1].getPosition()){
-					window.close();	
-				}
-			}
-
 			//print result
 			scoreText.setString("Score: " + to_string(snake.size()-2) + "	" + "Level: " + to_string((snake.size()-2) / 5));
         }
 
+		for(int i=2;i<snake.size();i++){
+			if(snake[i].getPosition() == snake[1].getPosition()){
+				gameOver = true;	
+			}
+		}
+
+		for(int i=0;i<wall.size();i++){
+			if(wall[i].getPosition() == snake[1].getPosition())
+				gameOver = true;
+		}
+		
+		//清除畫面
         window.clear();
-        for (int i = 0; i < snake.size(); i++)
-            window.draw(snake[i]);
-		for(int i=0;i<wall.size();i++)
-			window.draw(wall[i]);
-		window.draw(scoreText);
+		
+		//顯示畫面
+		if(gameOver){
+			window.draw(gameText);
+			window.draw(endText);
+		}else{
+        	for (int i = 0; i < snake.size(); i++)
+            	window.draw(snake[i]);
+			for(int i=0;i<wall.size();i++)
+				window.draw(wall[i]);
+			window.draw(scoreText);
+		}
         window.display();
     }
 
     return 0;
 }
-

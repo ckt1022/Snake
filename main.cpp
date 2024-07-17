a0a7e87a6d759dc26a9a47425816bdc481c04866
ckt1022 <gary.chen.1022@gmail.com>
2024-07-17 16:03:34 +0800

    起始介面、新增地圖

--- main.cpp
+++ main.cpp
@@ -12,43 +12,38 @@
 
 using namespace sf;
 using namespace std;
-
+RectangleShape generateRectangle(int size,int x,int y,Color a){
+	RectangleShape temp;
+	temp.setSize(Vector2f(size,size));
+	temp.setPosition(x,y);
+	temp.setFillColor(a);
+	return temp;
+}
 vector<RectangleShape> generateWall(int wallSize,int wigth,int length,int id){
 	vector<RectangleShape> wall;
 
-	for(int x = 0;x<wigth;x+=wallSize){
-		RectangleShape temp;
-		temp.setSize(Vector2f(wallSize,wallSize));
-		temp.setPosition(x,wigth-wallSize);
-		temp.setFillColor(Color::Cyan);
-		wall.push_back(temp);
+	for(int x = 0;x<wigth;x+=wallSize)
+		wall.push_back(generateRectangle(wallSize,x,wigth-wallSize,Color::Cyan));
 
-	}
-	if(id == 0){
+	if(id == 1){
 		for(int x=0;x<wigth;x+=wallSize){
 			for(int y=0;y<length - wallSize;y+=wallSize){
-				if(x == 0 || x == wigth-wallSize || y == 0 || y == length-wallSize){
-					RectangleShape temp;
-					temp.setSize(Vector2f(wallSize,wallSize));
-					temp.setPosition(x,y);
-					temp.setFillColor(Color::Cyan);
-
-					wall.push_back(temp);
-				}
+				if(x == 0 || x == wigth-wallSize || y == 0 || y == length-wallSize)
+					wall.push_back(generateRectangle(wallSize,x,y,Color::Cyan));
 			}
 		}
-	}else if(id == 1){
+	}else if(id == 2){
 		for(int x = 0;x < wigth;x += wallSize){
 			for(int y = 0;y<wigth;y+=wallSize){
-				if((x == 100 && y > 100) || (y == 600 && x > 300) || (y == 100 && (x >=100 && x < 500))){
-					RectangleShape temp;
-					temp.setSize(Vector2f(wallSize,wallSize));
-					temp.setPosition(x,y);
-					temp.setFillColor(Color::Cyan);
-
-					wall.push_back(temp);
-
-				}
+				if((x == 100 && y > 100) || (y == 600 && x > 300) || (y == 100 && (x >=100 && x < 500)))
+					wall.push_back(generateRectangle(wallSize,x,y,Color::Cyan));
+			}
+		}	
+	}else if(id == 3){
+		for(int x = 0;x < wigth;x += wallSize){
+			for(int y = 0;y < wigth;y += wallSize){
+				if(((x >= 0 && x<280) || (x>=500 && x<=wigth)) && y == 380 || ((y >= 0 && y<280) || (y>=500 && y<=wigth)) && x == 380)
+					wall.push_back(generateRectangle(wallSize,x,y,Color::Cyan));
 			}
 		}	
 	}
@@ -74,14 +69,14 @@ int main()
     vector<RectangleShape> snake;
 	
     RectangleShape food;
-	int snakeSize = 20;
+	int  snakeSize = 20;
     food.setSize(Vector2f(snakeSize,snakeSize));
     food.setPosition(rand() % (wigth/snakeSize) * snakeSize, rand() % (wigth/snakeSize) * snakeSize);
     food.setFillColor(Color::Green);
 
     RectangleShape ori;
     ori.setSize(Vector2f(snakeSize, snakeSize));
-    ori.setPosition(wigth/2-snakeSize/2-10,wigth/2-snakeSize/2-10);
+    ori.setPosition(wigth/2-snakeSize/2-10.0,wigth/2-snakeSize/2-10.0);
     ori.setFillColor(Color::Red);
 
     snake.push_back(food);
@@ -95,19 +90,35 @@ int main()
     }
 
 	//顯示訊息設定
+	//起始畫面
+	bool start = true;
+	Text startText("Snake Game",font,60);
+	startText.setPosition(175,350);
+	Text option1("	Press 'Enter' to start",font,25);
+	option1.setPosition(200,425);
+	option1.setFillColor(Color::Blue);
+
+	//得分訊息
 	Text scoreText;
     scoreText.setFont(font);
 	scoreText.setPosition(20,810);
     scoreText.setCharacterSize(24);
     scoreText.setFillColor(Color::White);
 	
+	//結束訊息
 	Text gameText("Game Over !!",font,60);
 	gameText.setPosition(175,350);
 	gameText.setFillColor(Color::Red);
 
-	Text endText("Pressed 'Space' to start a new game",font,25);
+	Text endText("Press 'Space' to start a new game\n	Press 'LShift' to home page",font,25);
 	endText.setPosition(150,425);
-	endText.setFillColor(Color::Blue);
+	endText.setFillColor(Color::White);
+
+	Text endScore;
+	endScore.setFont(font);
+	endScore.setCharacterSize(20);
+	endScore.setPosition(10,10);
+	endScore.setFillColor(Color::White);
 
 	//main window loop
     while (window.isOpen())
@@ -116,7 +127,10 @@ int main()
         Event event;
 	    while (window.pollEvent(event))
         {
-			if(gameOver && Keyboard::isKeyPressed(Keyboard::Space)){
+            if (event.type == Event::Closed)
+                window.close();
+			//起始與結束介面邏輯判斷
+			if(gameOver && Keyboard::isKeyPressed(Keyboard::Space) || (start && Keyboard::isKeyPressed(Keyboard::Enter))){
 				snake.erase(snake.begin()+2,snake.end());
 				snake[1].setSize(Vector2f(snakeSize, snakeSize));
     			snake[1].setPosition(wigth/2-snakeSize/2-10,wigth/2-snakeSize/2-10);
@@ -131,10 +145,12 @@ int main()
 				snake[0].setPosition(tempX,tempY);
 
 				gameOver = false;
+				if(start)
+					start = false;
 			}
-            if (event.type == Event::Closed)
-                window.close();
-			
+			if(gameOver && Keyboard::isKeyPressed(Keyboard::RShift))
+				start = true;
+
 			//snake head move
             int x = snake[1].getPosition().x;
             int y = snake[1].getPosition().y;
@@ -162,11 +178,8 @@ int main()
 					
 				snake[0].setPosition(tempX,tempY);
 
-                RectangleShape tail;
-                tail.setSize(Vector2f(snakeSize, snakeSize));
-                tail.setFillColor(Color::White);
-                tail.setPosition(snake[snake.size() - 1].getPosition());
-                snake.push_back(tail);
+				//生成尾巴
+                snake.push_back(generateRectangle(snakeSize,snake[snake.size() - 1].getPosition().x,snake[snake.size() - 1].getPosition().y,Color::White));
             }
 
 			//snake move
@@ -196,7 +209,12 @@ int main()
         window.clear();
 		
 		//顯示畫面
-		if(gameOver){
+		if(start){
+			window.draw(startText);
+			window.draw(option1);
+		}else if(gameOver){
+			endScore.setString("Final Score: "+ to_string(snake.size()-2) + "	 Level: " + to_string((snake.size())/5));
+			window.draw(endScore);
 			window.draw(gameText);
 			window.draw(endText);
 		}else{
